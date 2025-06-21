75e50710781c3b5d76a6c3f1fc30d2c2203e1811
Jecob Snyder <x0jxc0b315@gmail.com>
2025-06-21 00:05:42 -0400

    added comments to api script, set print statement to extract first 5 dictionary items to prevent system overload, in order to test JSON data for proper intention

--- src/apiendpoint.py
+++ src/apiendpoint.py
@@ -5,10 +5,11 @@ from .config import API_URL, THRESHOLD
 API_KEY = os.getenv("MY_API_KEY")  # Set this in your environment
 
 def fetch_tokens():
-    headers = {"Authorization": f"Bearer {API_KEY}"}
-    response = requests.get(API_URL, headers=headers)
-    response.raise_for_status()
-    data = response.json()
+    headers = {"Authorization": f"Bearer {API_KEY}"} #Set the API key in the headers
+    response = requests.get(API_URL, headers=headers) # Fetch data from the API URL
+    response.raise_for_status() # Raise Error for bad responses w built in method
+    data = response.json() # Convert response to JSON
+    print(data[:5])  # Only prints the first 5 tokens
     # Filter tokens by market cap threshold
     tokens = [t for t in data if t["market_cap"] >= THRESHOLD]
     return tokens
