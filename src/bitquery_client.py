00255d9636b1019c6add67087e8c449e8aa7e8c5
Jecob Snyder <x0jxc0b315@gmail.com>
2025-06-23 19:33:08 -0400

    relocate print test statement, remain commented out for commit

--- src/bitquery_client.py
+++ src/bitquery_client.py
@@ -134,6 +134,7 @@ def find_tokens_exceeding_market_cap(threshold=30000, min_times=2, since_days=30
         # print(mint, caps)
         if all(cap == 0 for cap in caps):
             continue  # Skip tokens with only zero values
+        print(mint, caps)  # This will only print tokens that are not skipped
         if broke_above_twice(caps, threshold):
             hourly_history = get_last_n_hourly_market_caps(mint, n=3)
             results.append({
